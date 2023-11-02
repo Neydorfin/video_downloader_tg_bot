7f93ca9f1e4860bfdac7301d6ac067f76e683a44
Neydorfin <iongalin.02@gmail.com>
2023-11-02 16:02:26 +0300

    tg-bot updated button added

--- tg_bot/core.py
+++ tg_bot/core.py
@@ -1,122 +1,141 @@
 import telebot
-from telebot.types import Message
+from telebot.types import Message, CallbackQuery
 from telebot import custom_filters
 from telebot.storage import StateMemoryStorage
 from config import BotSettings
 from common.states import States
 from common.text import TeleText
+from utils.keyboard import Buttons
 
 state_storage = StateMemoryStorage()
 bot = telebot.TeleBot(BotSettings.BOT_TOKEN, state_storage=state_storage)
 
 
+# МЕНЮ
+@bot.message_handler(state=States.main, commands=['main'])
+def main(message: Message) -> None:
+    bot.send_message(message.chat.id, TeleText.main, reply_markup=Buttons.remove)
+
+
+# команда запуска бота
 @bot.message_handler(commands=['start'])
 def start(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.welcome)
+    bot.send_message(message.chat.id, TeleText.welcome, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.main, message.chat.id)
     main(message)
 
 
+# команда вызова помощи
 @bot.message_handler(state="*", commands=['help'])
 def help(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.help)
+    bot.send_message(message.chat.id, TeleText.help, reply_markup=Buttons.remove)
 
 
+# команда вызова настроек
 @bot.message_handler(commands=['custom'])
 def custom(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.custom)
+    bot.send_message(message.chat.id, TeleText.custom, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.custom, message.chat.id)
 
 
-@bot.message_handler(state=States.main, commands=[])
-def main(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.main)
-
-
+# команда выбора минимального разрешение видео
 @bot.message_handler(state=States.custom, commands=['low'])
 def custom_low(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.custom_low)
+    bot.send_message(message.chat.id, TeleText.custom_low, reply_markup=Buttons.low_markup)
 
 
+# команда выбора максимального разрешение видео
 @bot.message_handler(state=States.custom, commands=['high'])
 def custom_high(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.custom_high)
+    bot.send_message(message.chat.id, TeleText.custom_high, reply_markup=Buttons.high_markup)
 
 
+# команда по поводу получение доп. информации про выидео
 @bot.message_handler(state=States.custom, commands=['info'])
 def custom_info(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.custom_info)
+    bot.send_message(message.chat.id, TeleText.custom_info, reply_markup=Buttons.switch_markup)
 
 
-@bot.message_handler(state=[States.custom, States.resolution_select], commands=['cancel'])
+# команда отмены
+@bot.message_handler(state="*", commands=['cancel'])
 def cancel(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.cancel)
+    bot.send_message(message.chat.id, TeleText.cancel, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.main, message.chat.id)
     main(message)
 
 
+# пользователб выбирает платформу с которой он будет скачивать видео
 @bot.message_handler(state=States.main, commands=['download'])
-def start_download(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.platform_select)
+def platform_select(message: Message) -> None:
+    bot.send_message(message.chat.id, TeleText.platform_select, reply_markup=Buttons.platform_markup)
     bot.set_state(message.from_user.id, States.platform_select, message.chat.id)
 
 
+# читаем ссылку от пользователя
 @bot.message_handler(state=States.platform_select)
-def platform_select(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.video_select)
+def video_select(message: Message) -> None:
+    bot.send_message(message.chat.id, TeleText.video_select, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.video_select, message.chat.id)
 
 
+# выво информации об видео с вопросом о продолжение скачивание
 @bot.message_handler(state=States.video_select)
-def video_select(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.about_video)
+def about_video(message: Message) -> None:
+    bot.send_message(message.chat.id, TeleText.about_video, reply_markup=Buttons.answer_markup)
     bot.set_state(message.from_user.id, States.about_video, message.chat.id)
 
 
+# выбор разрешение видео
 @bot.message_handler(state=States.about_video)
-def about_video(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.resolution_select)
+def resolution_select(message: Message) -> None:
+    bot.send_message(message.chat.id, TeleText.resolution_select, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.resolution_select, message.chat.id)
 
 
+# выбор минимального разрешение
 @bot.message_handler(state=States.resolution_select, commands=['low'])
 def low(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.low)
+    bot.send_message(message.chat.id, TeleText.low, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.download, message.chat.id)
     download(message)
 
 
+# выбор максимального разрешение
 @bot.message_handler(state=States.resolution_select, commands=['high'])
 def high(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.high)
+    bot.send_message(message.chat.id, TeleText.high, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.download, message.chat.id)
     download(message)
 
 
+# выбор разрешение по умолчанию
 @bot.message_handler(state=States.resolution_select, commands=['default'])
 def default(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.default)
+    bot.send_message(message.chat.id, TeleText.default, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.download, message.chat.id)
     download(message)
 
 
+# скачивание видео
 @bot.message_handler(state=States.download)
 def download(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.start_download)
+    bot.send_message(message.chat.id, TeleText.start_download, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.video_maker, message.chat.id)
     video_maker(message)
 
 
+# обработка видео
 @bot.message_handler(state=States.video_maker)
 def video_maker(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.video_processing)
+    bot.send_message(message.chat.id, TeleText.video_processing, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.send_video, message.chat.id)
     send_video(message)
 
 
+# отправка видео
 @bot.message_handler(state=States.send_video)
 def send_video(message: Message) -> None:
-    bot.send_message(message.chat.id, TeleText.sending_video)
+    bot.send_message(message.chat.id, TeleText.sending_video, reply_markup=Buttons.remove)
     bot.set_state(message.from_user.id, States.main, message.chat.id)
     main(message)
 
@@ -127,6 +146,7 @@ if __name__ == '__main__':
         telebot.types.BotCommand("start", "Запуск бота"),
         telebot.types.BotCommand("help", "Помощь по боту"),
         telebot.types.BotCommand("custom", "Настройка бота"),
-        telebot.types.BotCommand("download", "Начать скачивать видео")
+        telebot.types.BotCommand("download", "Начать скачивать видео"),
+        telebot.types.BotCommand("cancel", "Назад в Меню"),
     ])
     bot.polling()
