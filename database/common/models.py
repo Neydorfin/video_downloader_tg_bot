e7070941d16ce28e6f8f6130a3a38671b5b481eb
Neydorfin <iongalin.02@gmail.com>
2023-11-09 12:25:36 +0300

    model history updated

--- database/common/models.py
+++ database/common/models.py
@@ -15,23 +15,38 @@ class User(BaseModel):
 
     user_id = pw.IntegerField(primary_key=True)
     chat_id = pw.IntegerField()
-    username = pw.CharField(max_length=150)
+    username = pw.TextField(null=True)
+    first_name = pw.TextField(null=True)
+    last_name = pw.TextField(null=True)
 
 
 class History(BaseModel):
-    user_id = pw.PrimaryKeyField(User.user_id)
-    history_id = pw.IntegerField()
+    user_id = pw.ForeignKeyField(User.user_id, null=True)
+    history_id = pw.AutoField()
     created_at = pw.DateField(default=datetime.now())
-    link = pw.TextField()
-    video_name = pw.TextField()
-    video_views = pw.IntegerField()
-    video_size = pw.IntegerField()
-    message = pw.TextField()
+    link = pw.TextField(null=True)
+    video_id = pw.TextField(null=True)
+    title = pw.TextField(null=True)
+    time = pw.TimeField(null=True)
+    platform = pw.TextField(null=True)
+    resolution = pw.TextField(null=True)
+    thumbnail = pw.TextField(null=True)
+    video_views = pw.IntegerField(null=True)
+    video_size = pw.IntegerField(null=True)
+    _140 = pw.TextField(null=True)
+    _242 = pw.TextField(null=True)
+    _18 = pw.TextField(null=True)
+    _244 = pw.TextField(null=True)
+    _22 = pw.TextField(null=True)
+    _137 = pw.TextField(null=True)
+    _271 = pw.TextField(null=True)
+    _313 = pw.TextField(null=True)
 
 
 class UserConfig(BaseModel):
     class Meta:
         db_table = "User_Config"
+
     user_id = pw.PrimaryKeyField(User.user_id)
     low = pw.CharField(max_length=10)
     high = pw.CharField(max_length=10)
