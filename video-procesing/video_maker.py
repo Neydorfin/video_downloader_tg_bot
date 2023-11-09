3715b50f38b9935d5ef7365ca858e29dfa9ad41b
Neydorfin <iongalin.02@gmail.com>
2023-11-09 20:13:44 +0300

    video_maker.py fps removed

--- video-procesing/video_maker.py
+++ video-procesing/video_maker.py
@@ -1,9 +1,9 @@
 import moviepy.editor as mpe
 
 
-def combine_audio(data, fps=30):
+def combine_audio(data):
     output = f"resources/video/{data.title}.mp4"
     my_clip = mpe.VideoFileClip(data.vidname)
     audio_background = mpe.AudioFileClip(data.audname)
     final_clip = my_clip.set_audio(audio_background)
-    final_clip.write_videofile(output, fps=fps)
+    final_clip.write_videofile(output)
