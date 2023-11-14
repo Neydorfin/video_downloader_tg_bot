1020f37e4ba675850bd5f0340b7b74783d098182
Neydorfin <iongalin.02@gmail.com>
2023-11-14 12:59:37 +0300

    updated video downloader

--- site_api_requests/youtube_requests/download.py
+++ site_api_requests/youtube_requests/download.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 
 def default_download(data) -> None:
     video = YouTube(data.link)
-    youtube_object = video.streams.get_by_itag(22)
+    youtube_object = video.streams.get_highest_resolution()
     try:
         youtube_object.download(output_path="resources/video", filename=f"{data.video_id}.mp4")
     except:
--- video-procesing/video_maker.py
+++
@@ -1,9 +0,0 @@
-import moviepy.editor as mpe
-
-
-def combine_audio(data):
-    output = f"resources/video/{data.title}.mp4"
-    my_clip = mpe.VideoFileClip(data.vidname)
-    audio_background = mpe.AudioFileClip(data.audname)
-    final_clip = my_clip.set_audio(audio_background)
-    final_clip.write_videofile(output)
