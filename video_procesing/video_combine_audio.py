46b7761d9a7f82129c6b1ba11b62c73229c349c1
Neydorfin <iongalin.02@gmail.com>
2023-11-21 15:51:57 +0300

    video_combine_audio.py updated

--- video_procesing/video_combine_audio.py
+++ video_procesing/video_combine_audio.py
@@ -1,9 +1,33 @@
 import moviepy.editor as mpe
+import argparse
 
 
-def combine_audio(video, audio, output):
-    output = f"resources/video/{output}.mp4"
-    my_clip = mpe.VideoFileClip(video)
-    audio_background = mpe.AudioFileClip(audio)
-    final_clip = my_clip.set_audio(audio_background)
-    final_clip.write_videofile(output)
+class VideoMaker:
+
+    # @staticmethod
+    # def combine_audio(video, audio, output):
+    #     output = f"resources/video/{output}.mp4"
+    #     # загрузка видео
+    #     video_clip = mpe.VideoFileClip(video)
+    #     # загрузка звука
+    #     audio_clip = mpe.AudioFileClip(audio)
+    #     # добавить звук к видео
+    #     final_clip = video_clip.set_audio(audio_clip)
+    #     # сохранить готовый клип
+    #     final_clip.write_videofile(output)
+
+    @staticmethod
+    def combine_audio(video, audio, output):
+        output = f"resources/video/{output}.mp4"
+        # загрузка видео
+        video_clip = mpe.VideoFileClip(video)
+        # загрузка звука
+        audio_clip = mpe.AudioFileClip(audio)
+        # используется конец видеоклипа
+        end = video_clip.end
+        # установка начала и конца аудиоклипа в параметрах `start` и `end`
+        audio_clip = audio_clip.subclip(0, end)
+        # добавить звук к видео
+        final_clip = video_clip.set_audio(audio_clip)
+        # сохранить готовый клип
+        final_clip.write_videofile(output)
